2363f5177f08aabaa1ce1e5c3a30c8dcf876267d
kutukov2000 <kutukov16.2005@gmail.com>
2023-06-18 21:29:48 +0300

    Add first card style

--- poker/MainWindow.xaml.cs
+++ poker/MainWindow.xaml.cs
@@ -15,14 +15,23 @@ using System.Windows.Shapes;
 
 namespace poker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public class Card
+    {
+        public string Suit { get; set; }//Масть
+        public string Rank { get; set; }//Значення карти
+
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            Button button=new Button();
+            button.Style = (Style)this.Resources["CardStyle"];
+            button.Content = "K";
+            button.Tag = "♥";
+            CardsPanel.Children.Add(button);
         }
     }
 }
