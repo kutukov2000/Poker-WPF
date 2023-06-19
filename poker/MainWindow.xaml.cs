5af7b1c4f53d8f717edb521777f1353a470a384f
kutukov2000 <kutukov16.2005@gmail.com>
2023-06-19 11:20:30 +0300

    Display cards

--- poker/MainWindow.xaml.cs
+++ poker/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.IO;
+using System.Globalization;
 
 namespace poker
 {
@@ -31,14 +32,206 @@ namespace poker
     public partial class MainWindow : Window
     {
         List<Card> cards;
+        HashSet<Card> selectedCards=new HashSet<Card>();
+        Random random = new Random();
         public MainWindow()
         {
             InitializeComponent();
 
+            //GenerateDeckOfCard();
+
+            ReadDeckOfCards();
+
+            StartGame();
+        }
+        public void StartGame()
+        {
+            playerDock.Children.Clear();
+            playerDock2.Children.Clear();
+            playerDock3.Children.Clear();
+            playerDock4.Children.Clear();
+            gamesCardDock.Children.Clear(); 
+
+            CardsToPlayer(playerDock, 2);
+            CardsToPlayer(playerDock2, 2);
+            CardsToPlayer(playerDock3, 2);
+            CardsToPlayer(playerDock4, 2);
+            CardsToPlayer(gamesCardDock, 3);
+        }
+        public void CardsToPlayer(DockPanel panel, int countOfCards)
+        {
+            for (int i = 0; i < countOfCards; i++)
+            {
+                Card card = SelectCard();
+                AddButton(panel, card);
+                selectedCards.Add(card);
+            }
+        }
+        public Card SelectCard()
+        {
+            Card card = cards[random.Next(0, cards.Count - 1)];
+            if (CheckCard(card))
+                return card;
+
+            return SelectCard();
+        }
+        public bool CheckCard(Card card)
+        {
+            if (selectedCards.Count != 0)
+            {
+                foreach (var item in selectedCards)
+                {
+                    if (card == item)
+                        return false;
+                }
+            }
+            return true;
+        }
+        public void AddButton(DockPanel panel, Card card)
+        {
+            Button button = new Button();
+            button.Style = (Style)this.Resources["CardStyle"];
+
+            button.Content = card.Rank;
+            button.Tag = card.Suit;
+
+            if (card.Suit == "♥" || card.Suit == "♦")
+                button.Foreground = Brushes.Red;
+            else
+                button.Foreground = Brushes.Black;
+
+            panel.Children.Add(button);
+        }
+        public void ReadDeckOfCards()
+        {
             string json = File.ReadAllText("cards.json");
 
-            // Розбір JSON-рядка і перетворення на об'єкт типу Card
             cards = JsonSerializer.Deserialize<List<Card>>(json);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(gamesCardDock.Children.Count<5)
+                CardsToPlayer(gamesCardDock, 1);
+        }
+        public void GenerateDeckOfCard()
+        {
+            HashSet<Card> cards = new HashSet<Card>();
+            for (int i = 2; i < 11; i++)
+            {
+                cards.Add(new Card
+                {
+                    Suit = "♠",
+                    Rank = i.ToString(),
+                });
+            }
+            cards.Add(new Card
+            {
+                Suit = "♠",
+                Rank = "J",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♠",
+                Rank = "Q",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♠",
+                Rank = "K",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♠",
+                Rank = "A",
+            });
+
+            for (int i = 2; i < 11; i++)
+            {
+                cards.Add(new Card
+                {
+                    Suit = "♥",
+                    Rank = i.ToString(),
+                });
+            }
+            cards.Add(new Card
+            {
+                Suit = "♥",
+                Rank = "J",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♥",
+                Rank = "Q",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♥",
+                Rank = "K",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♥",
+                Rank = "A",
+            });
+
+            for (int i = 2; i < 11; i++)
+            {
+                cards.Add(new Card
+                {
+                    Suit = "♦",
+                    Rank = i.ToString(),
+                });
+            }
+            cards.Add(new Card
+            {
+                Suit = "♦",
+                Rank = "J",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♦",
+                Rank = "Q",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♦",
+                Rank = "K",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♦",
+                Rank = "A",
+            });
+
+            for (int i = 2; i < 11; i++)
+            {
+                cards.Add(new Card
+                {
+                    Suit = "♣",
+                    Rank = i.ToString(),
+                });
+            }
+            cards.Add(new Card
+            {
+                Suit = "♣",
+                Rank = "J",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♣",
+                Rank = "Q",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♣",
+                Rank = "K",
+            });
+            cards.Add(new Card
+            {
+                Suit = "♣",
+                Rank = "A",
+            });
 
             foreach (var card in cards)
             {
@@ -48,139 +241,16 @@ namespace poker
                 button.Tag = card.Suit;
                 CardsPanel.Children.Add(button);
             }
+
+            string jsonString = JsonSerializer.Serialize(cards);
+
+            File.WriteAllText("cards.json", jsonString);
         }
 
-        //    public void GenerateDeckOfCard()
-        //    {
-        //        HashSet<Card> cards = new HashSet<Card>();
-        //        for (int i = 1; i < 11; i++)
-        //        {
-        //            cards.Add(new Card
-        //            {
-        //                Suit = "♠",
-        //                Rank = i.ToString()
-        //            }) ;
-        //        }
-        //        for (int i = 1; i < 11; i++)
-        //        {
-        //            cards.Add(new Card
-        //            {
-        //                Suit = "♥",
-        //                Rank = i.ToString()
-        //            });
-        //        }
-        //        for (int i = 1; i < 11; i++)
-        //        {
-        //            cards.Add(new Card
-        //            {
-        //                Suit = "♦",
-        //                Rank = i.ToString()
-        //            });
-        //        }
-        //        for (int i = 1; i < 11; i++)
-        //        {
-        //            cards.Add(new Card
-        //            {
-        //                Suit = "♣",
-        //                Rank = i.ToString()
-        //            });
-        //        }
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♣",
-        //            Rank = "J"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♣",
-        //            Rank = "Q"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♣",
-        //            Rank = "K"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♣",
-        //            Rank = "A"
-        //        });
-
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♠",
-        //            Rank = "J"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♠",
-        //            Rank = "Q"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♠",
-        //            Rank = "K"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♠",
-        //            Rank = "A"
-        //        });
-
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♥",
-        //            Rank = "J"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♥",
-        //            Rank = "Q"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♥",
-        //            Rank = "K"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♥",
-        //            Rank = "A"
-        //        });
-
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♦",
-        //            Rank = "J"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♦",
-        //            Rank = "Q"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♦",
-        //            Rank = "K"
-        //        });
-        //        cards.Add(new Card
-        //        {
-        //            Suit = "♦",
-        //            Rank = "A"
-        //        });
-        //        foreach (var card in cards)
-        //        {
-        //            Button button = new Button();
-        //            button.Style = (Style)this.Resources["CardStyle"];
-        //            button.Content = card.Rank;
-        //            button.Tag = card.Suit;
-        //            CardsPanel.Children.Add(button);
-        //        }
-
-        //        string jsonString=JsonSerializer.Serialize(cards);
-
-        //        File.WriteAllText("cards.json", jsonString);
-        //    //}
-        //}
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+        }
     }
 }
+
